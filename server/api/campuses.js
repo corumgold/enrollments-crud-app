a5b8007abf704a95b905c38cf52dfe919cd6c2da
Corum Gold <goldcorum@gmail.com>
2022-08-29 08:47:46 -0400

    Add individual campus GET API

--- server/api/campuses.js
+++ server/api/campuses.js
@@ -11,4 +11,13 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+//GET /campuses/:id
+router.get("/:id", async (req, res, next) => {
+  try {
+    res.send(await Campus.findByPk({ include: Student }));
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
