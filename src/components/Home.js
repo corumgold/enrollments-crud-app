f14f8df9f4df685982a8b15d2efae9c8eb62abee
Corum Gold <goldcorum@gmail.com>
2022-08-31 13:25:45 -0400

    Begin styling home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,18 @@ import React from "react";
 const Home = () => {
   return (
     <div className="home flex-column center">
-      <h1>Summer 2022 Junior Phase Final Project</h1>
+      <h1>Junior Phase Final Project</h1>
       <h2>By Cory Gold</h2>
-      <h3>(Please Choose One of the Above Options)</h3>
+      <h3>August, 2022</h3>
+      <p>
+        This project is designed to mimic the interface of an application that
+        manages different school campuses as well as the students enrolled.
+      </p>
+      <p>
+        This application is a single page application wherein you can create,
+        modify, and delete school campuses as well as students. Users are also
+        able to assign students to different schools.
+      </p>
     </div>
   );
 };
